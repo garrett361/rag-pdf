863883fe0444a720c2395fdbd8b0209df6b89dfb
Garrett Goon <garrett.goon@hpe.com>
2024-09-13 13:53:29 -0400

    add base model template

--- rag/_defaults.py
+++ rag/_defaults.py
@@ -1,3 +1,4 @@
 DEFAULT_HF_EMBED_MODEL = "BAAI/bge-base-en-v1.5"
 DEFAULT_HF_CHAT_MODEL = "TinyLlama/TinyLlama-1.1B-Chat-v0.4"
 DEFAULT_CHUNK_STRAT = "by_title"
+DEFAULT_HF_CHAT_TEMPLATE = "\n<|user|>:{}</s>\n<|assistant|>:"
