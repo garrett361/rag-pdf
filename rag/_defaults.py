5e62e04f7b896c8018bacdf62eb3c1ccee1e422e
Garrett Goon <garrett.goon@hpe.com>
2024-09-17 13:34:11 -0400

    default max bsz and comment

--- rag/_defaults.py
+++ rag/_defaults.py
@@ -9,3 +9,5 @@ You are an assistant for answering questions.
 You are given the extracted parts of a long document and a question. Provide a conversational answer.
 If you don't know the answer, just say "I do not know." Don't make up an answer.
 """).strip("\n")
+# The embedding model hosted on houston errors out at larger batch sizes
+DEFAULT_MAX_EMBED_BSZ = 32
