842930206b05784ebfde3fdc8affcdc267299d01
Garrett Goon <garrett.goon@hpe.com>
2024-09-23 17:18:13 -0400

    edit uninformative prompt

--- rag/_defaults.py
+++ rag/_defaults.py
@@ -12,12 +12,13 @@ If you don't know the answer, just say "I do not know." Don't make up an answer.
 # The embedding model hosted on houston errors out at larger batch sizes
 DEFAULT_MAX_EMBED_BSZ = 32
 DEFAULT_MAX_NEW_TOKS = 500
+DEFAULT_ALPHA = 0.95
 UNINFORMATIVE_PROMPT = dedent("""
-Does the text extract below from a parsed PDF look like it's a part of the table of contents, or repeated header and footer, or a random gibberish of characters?
+Below is a text extract from a document. I will ask you whether the extract looks informative.
 
-{context}
+Extract: {context}
 
-Only respond with "yes" or "no".
+Headers, footers, and tables of contents are examples of non-informative extracts. Did the above extract look informative? Only respond with "yes" or "no".
 """).strip("\n")
 
 QA_PROMPT = dedent("""
