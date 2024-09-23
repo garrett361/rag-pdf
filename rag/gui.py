291c670de40d705f2bcb493dc7100e84bf7bd58d
Garrett Goon <garrett.goon@hpe.com>
2024-09-23 11:38:57 -0400

    gui changes

--- rag/gui.py
+++ rag/gui.py
@@ -3,16 +3,13 @@ import os
 import pathlib
 
 import streamlit as st
-from llama_index.core import Settings, get_response_synthesizer
-from llama_index.core.postprocessor import SimilarityPostprocessor
-from llama_index.core.query_engine import RetrieverQueryEngine
-from llama_index.core.retrievers import VectorIndexRetriever
+from llama_index.core import Settings
 from llama_index.core.vector_stores.types import MetadataFilter, MetadataFilters
 from llama_index.llms.openllm import OpenLLM
 from transformers import AutoTokenizer
 
 from rag._defaults import DEFAULT_HF_CHAT_MODEL, DEFAULT_HF_EMBED_MODEL, DEFAULT_MAX_NEW_TOKS
-from rag.query import get_local_llm, load_data
+from rag.query import create_retriever, get_llm_answer, get_local_llm, get_nodes, load_data
 
 static_path = pathlib.Path(__file__).parent.joinpath("static")
 print(f"{static_path=}")
@@ -37,10 +34,10 @@ parser.add_argument(
     help="HTTP path to model endpoint, if serving",
 )
 parser.add_argument(
-    "--top-k",
+    "--top-k-retriever",
     default=5,
     type=int,
-    help="top k results",
+    help="top k for retreiver",
 )
 parser.add_argument(
     "--max-new-tokens",
@@ -103,7 +100,7 @@ st.session_state.temp = 0.2
 st.session_state.top_p = 0.8
 st.session_state.max_length = 250
 st.session_state.cutoff = args.cutoff
-st.session_state.top_k = args.top_k
+st.session_state.top_k_retriever = args.top_k_retriever
 
 
 @st.cache_data
@@ -111,7 +108,7 @@ def load_chat_model(
     temp=st.session_state.temp,
     max_length=st.session_state.max_length,
     top_p=st.session_state.top_p,
-):
+) -> None:
     generate_kwargs = {
         "do_sample": True,
         "temperature": temp,
@@ -120,7 +117,7 @@ def load_chat_model(
     }
     if args.chat_model_endpoint:
         st.write(f"Using model endpoint: {args.model_name}")
-        llm = OpenLLM(
+        Settings.llm = OpenLLM(
             model=args.model_name,
             api_base=args.chat_model_endpoint,
             api_key="fake",
@@ -129,28 +126,13 @@ def load_chat_model(
         )
     else:
         tokenizer = AutoTokenizer.from_pretrained(args.model_name)
-        llm = get_local_llm(
+        Settings.llm = get_local_llm(
             model_name=args.model_name,
             tokenizer=tokenizer,
             use_4_bit_quant=False,
             generate_kwargs=generate_kwargs,
         )
         st.write(f"Using local model: {args.model_name}")
-    Settings.llm = llm
-    return None
-
-
-def create_query_engine(filters=None, cutoff=st.session_state.cutoff, top_k=st.session_state.top_k):
-    retriever = VectorIndexRetriever(index=index, similarity_top_k=top_k, filters=filters)
-    # configure response synthesizer
-    response_synthesizer = get_response_synthesizer(response_mode="no_text", streaming=False)
-    query_engine = RetrieverQueryEngine.from_args(
-        retriever=retriever,
-        response_synthesizer=response_synthesizer,
-        node_postprocessors=[SimilarityPostprocessor(similarity=cutoff)],
-    )
-    # query_engine = index.as_query_engine(similarity_top_k=args.top_k, streaming=True)
-    return query_engine
 
 
 welcome_message = "Hello, I am HPE Document chat. \n\n Please ask me any questions related to the documents listed below. If there are no documents listed, please select a tag below to filter."
@@ -237,14 +219,16 @@ brief = "just generate the answer without a lot of explanations."
 
 def reload():
     with st.spinner(f"Loading {args.model_name} q&a model..."):
-        llm = load_chat_model(
+        Settings.llm = load_chat_model(
             temp=st.session_state.temp,
             top_p=st.session_state.top_p,
             max_length=st.session_state.max_length,
         )
-    global query_engine
-    query_engine = create_query_engine(
-        cutoff=st.session_state.cutoff, top_k=st.session_state.top_k, filters=filters
+    global retriever
+    retriever = create_retriever(
+        cutoff=st.session_state.cutoff,
+        top_k_retriever=st.session_state.top_k_retriever,
+        filters=filters,
     )
 
 
@@ -255,7 +239,7 @@ def output_stream(llm_stream):
 
 with col1.expander("Settings"):
     temp = st.slider("Temperature", 0.0, 1.0, key="temp")
-    top_k = st.slider("Top K", 1, 25, key="top_k")
+    top_k_retriever = st.slider("Top K (Retriever)", 1, 25, key="top_k_retriever")
     cutoff = st.slider("Cutoff", 0.0, 1.0, key="cutoff")
     instructions = st.text_area("Prompt Instructions", default_instructions)
     st.button("Update Settings", on_click=reload())
@@ -266,52 +250,30 @@ if prompt := input_container.chat_input("Say something..."):
         st.write(prompt)
 
     print(f"Querying with prompt: {prompt}")
-    output = query_engine.query(prompt)
-    context_str = ""
-    for node in output.source_nodes:
-        print(f"Context: {node.metadata}")
-        context_str += node.text.replace("\n", "  \n")
-    text_qa_template_str_llama3 = f"""
-        <|begin_of_text|><|start_header_id|>user<|end_header_id|>
-        Context information is
-        below.
-        ---------------------
-        {context_str}
-        ---------------------
-        Using
-        the context information, answer the question: {prompt}
-        {instructions}
-        <|eot_id|><|start_header_id|>assistant<|end_header_id|>
-        """
-    llm = Settings.llm
+    nodes = get_nodes(prompt, retriever, reranker=None)
     if args.streaming:
-        output_response = llm.stream_complete(text_qa_template_str_llama3, formatted=True)
-        with chat_container.chat_message(
-            "assistant", avatar=str(static_path.joinpath("logo.jpeg"))
-        ):
-            response = st.write_stream(output_stream(output_response))
+        raise ValueError("Streaming not yet supported")
     else:
-        output_response = llm.complete(text_qa_template_str_llama3)
-        print(output_response)
         with chat_container.chat_message(
             "assistant", avatar=str(static_path.joinpath("logo.jpeg"))
         ):
-            response = st.write(output_response.text)
+            # TODO: @garrett.goon - fix tag placeholder
+            response = get_llm_answer(Settings.llm, "NZT", cutoff, top_k_retriever, query=prompt)
+            st.write(response)
 
     project = os.getenv("PPS_PROJECT_NAME", "default")
     doc_repo = os.getenv("DOCUMENT_REPO", "documents")
     proxy_url = os.getenv("PACH_PROXY_EXTERNAL_URL_BASE", "http://localhost:30080")
 
     with col2:
-        references = output.source_nodes
-        for i in range(len(references)):
-            title = references[i].node.metadata["Source"]
-            page = references[i].node.metadata["PageNumber"]
-            text = references[i].node.text
-            commit = references[i].node.metadata["Commit"]
-            doctag = references[i].node.metadata["Tag"]
+        for n in nodes:
+            title = n.metadata["Source"]
+            page = n.metadata["PageNumber"]
+            text = n.text
+            commit = n.metadata["Commit"]
+            doctag = n.metadata["Tag"]
             newtext = text.encode("unicode_escape").decode("unicode_escape")
-            out_title = f"**Source:** {title}  \n **Page:** {page}  \n **Similarity Score:** {round((references[i].score * 100),3)}% \n"
+            out_title = f"**Source:** {title}  \n **Page:** {page}  \n **Similarity Score:** {round((n.score * 100),3)}% \n"
             out_text = f"**Text:**  \n {newtext}  \n"
             title = title.replace(" ", "%20")
             if doctag:
